56369c6df945bfbae75804fc2e55dc7babd93aa5
keeks5456 <Akikogreen08@gmail.com>
2020-06-22 22:58:29 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :students, only: :index
+  get 'students/:id', to: 'students#index'
 end
+
+# The user is able to go to /students/2 to receive information on the student with an id of 2
+# The application should render the values from the student record in the database
